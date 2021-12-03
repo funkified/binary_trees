43a94f4bb1f3b1a3ff6e1dae19f273a332923c47
Sergio Vera <sergio.vera@icloud.com>
2021-12-03 21:51:38 +0000

    fix code

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -1,18 +1,21 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_size - measures the size of a binary tree
- * @tree: pointer to the root node of the tree to measure the size
- * Return: messure of size or 0 if NULL
+ * binary_tree_leaves - counts the leaves in a binary tree
+ * @tree: pointer to the root node of the tree to count the number of leaves
+ * Return: 0 or 1
  */
 
-size_t binary_tree_size(const binary_tree_t *tree)
+size_t binary_tree_leaves(const binary_tree_t *tree)
 {
-	size_t a, b;
+	size_t a;
+	size_t b;
 
 	if (!tree)
 		return (0);
-	a = binary_tree_size(tree->left);
-	b = binary_tree_size(tree->right);
-	return (a + b + 1);
+	if (!tree->left && !tree->right)
+		return (1);
+	a = binary_tree_leaves(tree->left);
+	b = binary_tree_leaves(tree->right);
+	return (a + b);
 }
